9a8c1f77609da8805c6d9fab2220b590b9bc0752
lingyiqun <lingyiqun@meituan.com>
2018-04-24 22:37:19 +0800

    使用parcel的环境搭建

---
+++ tests/test_add.js
@@ -0,0 +1,11 @@
+const expect = require("chai").expect
+
+describe("san test",function(){
+    it(
+        "test add",
+        function(){
+            expect(1+2).equal(3);
+        }
+    )
+}
+)
